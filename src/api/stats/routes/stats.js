60af5aec7a7f639ebb91e44b4cef4ac4dee10e3b
Alisher555-Noob <alirizojonkarimov@gamil.com>
2025-02-07 17:44:37 +0500

    Сообщение о том, что было обновлено

--- src/api/stats/routes/stats.js
+++ src/api/stats/routes/stats.js
@@ -1,12 +1,10 @@
 module.exports = {
-  routes: [
-    {
-      method: 'GET',
-      path: '/stats',
-      handler: 'stats.getStats',
-      config: {
-        auth: false,
-      },
-    },
-  ],
+  async getStats(ctx) {
+    try {
+      ctx.send({ message: "API работает!" });
+    } catch (error) {
+      console.error("Ошибка в API:", error); // Логирование ошибки
+      ctx.throw(500, "Ошибка сервера");
+    }
+  },
 };
